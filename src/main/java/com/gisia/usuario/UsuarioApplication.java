9df47201e3356bf0c349a0e067de54c014ec423a
Gísia <gisia.luan@gmail.com>
2026-02-27 19:52:33 -0300

    commit inicial

---
+++ src/main/java/com/gisia/usuario/UsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.gisia.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioApplication.class, args);
+	}
+
+}
