832dfd417d36807284769ce75d335bcd64075f79
Gísia <gisia.luan@gmail.com>
2026-02-28 17:23:02 -0300

    adicionado método que salva dados

---
+++ src/main/java/com/gisia/usuario/controller/UsuarioController.java
@@ -0,0 +1,24 @@
+package com.gisia.usuario.controller;
+
+import com.gisia.usuario.business.UsuarioService;
+import com.gisia.usuario.business.dto.UsuarioDTO;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO){
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+}
