70d32b06ce49de434e138ab31b1fb5728de5d475
Gísia <gisia.luan@gmail.com>
2026-05-14 15:51:50 -0300

    adicionada consulta na api externa do via_cep

--- src/main/java/com/gisia/usuario/controller/GlobalExceptionHandler.java
+++ src/main/java/com/gisia/usuario/controller/GlobalExceptionHandler.java
@@ -26,4 +26,8 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
